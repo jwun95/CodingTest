b8a69f6c9f1134c4ff4569976a25b4b480925ae4
Dartz <jwun95@naver.com>
2023-04-13 15:38:33 +0900

    기본 파일 수정

--- 백준/pythonBase.py
+++ 백준/pythonBase.py
@@ -2,9 +2,14 @@ import sys
 
 sys.stdin = open("../input.txt", "r")
 
-# N = int(sys.stdin.readline().rstrip())
-# data = sys.stdin.readline().rstrip()
-# N, M = map(int, sys.stdin.readline().split())
-# for _ in range(J):
-#     apples.append(int(sys.stdin.readline()))
-# data = list(map(int, input().split()))
+N = int(sys.stdin.readline().rstrip())
+data = sys.stdin.readline().rstrip()
+N, M = map(int, sys.stdin.readline().split())
+for _ in range(J):
+    apples.append(int(sys.stdin.readline()))
+data = list(map(int, input().split()))
+
+def solution():
+    pass
+
+print(solution())
