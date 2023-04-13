115d9480ddaeb2f430b45fb0b83c0296959a894c
Dartz <jwun95@naver.com>
2023-04-13 14:19:05 +0900

    기본 세팅

---
+++ 백준/pythonBase.py
@@ -0,0 +1,10 @@
+import sys
+
+sys.stdin = open("../input.txt", "r")
+
+# N = int(sys.stdin.readline().rstrip())
+# data = sys.stdin.readline().rstrip()
+# N, M = map(int, sys.stdin.readline().split())
+# for _ in range(J):
+#     apples.append(int(sys.stdin.readline()))
+# data = list(map(int, input().split()))
