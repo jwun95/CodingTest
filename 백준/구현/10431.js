91ccc5ead7a00f935002d7749fbe239f5288714b
Dartz <jwun95@naver.com>
2023-05-03 22:18:34 +0900

    10431.js

--- 백준/구현/10431.js
+++ 백준/구현/10431.js
@@ -1,3 +1,4 @@
+// 삽입 정렬
 const fs = require("fs");
 const stdin = (
   process.platform === "linux"
@@ -24,7 +25,22 @@ for (let i = 0; i < N; i++) {
 }
 
 classes.forEach((students) => {
+  const sortedClass = [students[1]];
   let count = 0;
-  let sor = [];
-  for (let i = 1; i < 21; i++) {}
+  let arrLength = 0;
+
+  for (let i = 2; i < 21; i++) {
+    arrLength = sortedClass.length;
+    if (sortedClass[arrLength - 1] > students[i]) {
+      while (arrLength && sortedClass[arrLength - 1] > students[i]) {
+        sortedClass[arrLength] = sortedClass[arrLength - 1];
+        sortedClass[arrLength - 1] = students[i];
+        count += 1;
+        arrLength -= 1;
+      }
+    } else {
+      sortedClass.push(students[i]);
+    }
+  }
+  console.log(`${students[0]} ${count}`);
 });
