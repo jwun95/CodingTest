f09d263e6fd0470f1fe676c7e54ae037b29ae9cb
jwun95 <jwun95@naver.com>
2023-06-21 13:39:07 +0900

    dfs

---
+++ 백준/dfs/dfs.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : fs.readFileSync("../input.txt").toString()
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const [N, M, P] = input()
+  .trim()
+  .split(" ")
+	.map((v) => parseInt(v));
