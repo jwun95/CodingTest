b0b285c700f64e4105b520076f0a4a1519e30985
jwun95 <jwun95@naver.com>
2023-09-08 17:44:54 +0900

    아기상어 임시저장

--- solution.js
+++ solution.js
@@ -45,3 +45,98 @@ while (true) {
     console.log("invalid");
   } else console.log("valid");
 }
+
+// 아기 상어
+
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : fs.readFileSync("./input.txt").toString()
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const N = Number(input().trim());
+const space = [];
+
+for (let n = 0; n < N; n++) {
+  space.push(
+    input()
+      .trim()
+      .split(" ")
+      .map((v) => Number(v))
+  );
+}
+
+const curr = [0, 0];
+
+for (let r = 0; r < N; r++) {
+  for (let c = 0; c < N; c++) {
+    if (space[r][c] === 9) {
+      space[r][c] = 0;
+      curr[0] = r;
+      curr[1] = c;
+    }
+  }
+}
+
+function bfs(curr, space, size) {
+  const dir = [
+    [-1, 0],
+    [0, -1],
+    [0, 1],
+    [1, 0],
+  ];
+
+  const visited = Array.from(Array(N), () => Array(N).fill(false));
+
+  visited[curr[0]][curr[1]] = true;
+  const queue = [[...curr, 0]];
+  while (queue.length) {
+    const [r, c, num] = queue.shift();
+
+    for (const d of dir) {
+      const nR = r + d[0];
+      const nC = c + d[1];
+
+      if (
+        nR >= 0 &&
+        nR < N &&
+        nC >= 0 &&
+        nC < N &&
+        !visited[nR][nC] &&
+        space[nR][nC] <= size
+      ) {
+        if (space[nR][nC] < size && 0 < space[nR][nC]) return [nR, nC, num + 1];
+        visited[nR][nC] = true;
+        queue.push([nR, nC, num + 1]);
+      }
+    }
+  }
+  return false;
+}
+
+let sharkSize = 2;
+let move = 0;
+let count = 0;
+
+while (true) {
+  const response = bfs(curr, space, sharkSize);
+  if (response === false) break;
+  console.log(response);
+  move += response[2];
+  curr[0] = response[0];
+  curr[1] = response[1];
+  space[response[0]][response[1]] = 0;
+  count++;
+  if (count === sharkSize) {
+    sharkSize++;
+    count = 0;
+  }
+}
+console.log(space);
+console.log(move);
